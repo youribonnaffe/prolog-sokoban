4feacb47bc22a5115ab46db08965de89e6ba44fa
youribm <youribm@4a8b9fd2-a42a-11dd-a9c7-49dec45ca499>
2008-10-27 20:09:05 +0000

    read level and call prolog

--- trunk/com.prolog.sokoban/src/com/prolog/sokoban/Level.java
+++ trunk/com.prolog.sokoban/src/com/prolog/sokoban/Level.java
@@ -6,31 +6,40 @@ import java.util.ArrayList;
 
 public class Level {
 	public static final int SIZE_LIAISON = 2;
-	
+
 	private ArrayList<String[]> liaisons;
-	
-	public Level () {
+	String caisses = "";
+	String perso = "";
+	String cibles = "";
+
+	public Level() {
 		liaisons = new ArrayList<String[]>();
 	}
-	
+
 	public void add(String piece1, String piece2) {
-		String[] liaison = {piece1, piece2};
+		String[] liaison = { piece1, piece2 };
 		liaisons.add(liaison);
 	}
-	
+
 	public void write(DataOutputStream os) throws IOException {
-		for( String [] liaison : liaisons ) {
-			os.writeBytes("liaison("+liaison[0]+","+liaison[1]+").");
+		for (String[] liaison : liaisons) {
+			os.writeBytes("liaison(" + liaison[0] + "," + liaison[1] + ").");
 			os.writeByte('\n');
 		}
 	}
-	
-	public String toString(){
+
+	public String toString() {
 		String s = "";
-		for( String [] liaison : liaisons ) {
-			s += "liaison("+liaison[0]+","+liaison[1]+").";
+		for (String[] liaison : liaisons) {
+			s += "liaison(" + liaison[0] + "," + liaison[1] + ").";
 			s += '\n';
 		}
+		s += "\n";
+		s += "Caisses : " + caisses;
+		s += "\n";
+		s += "Cibles : " + cibles;
+		s += "\n";
+		s += "Perso : " + perso;
 		return s;
 	}
 }
--- trunk/com.prolog.sokoban/src/com/prolog/sokoban/Prolog.java
+++ trunk/com.prolog.sokoban/src/com/prolog/sokoban/Prolog.java
@@ -1,25 +1,67 @@
 package com.prolog.sokoban;
 
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class Prolog {
 
 	static boolean stopProgress = false;
-	
+	public static final int NB_MAX_COUPS = 42;
+
 	public static void main(String[] args) {
 
-		if (args.length < 3) {
+		if (args.length < 4) {
 			System.err
-			.println("$1 = SWIPL path, $2 = .pl INPUT FILE, $3 = OUTPUT_FILE");
+					.println("$1 = SWIPL path, $2 = .pl INPUT FILE, $3 = OUTPUT_FILE, $4 = LEVEL_FILE");
 			return;
 		}
 
 		// params
-		String OUTPUT_FILE = args[2];
-		String SWIPL = args[0];
-		String INPUT_FILE = args[1];
+		String output_file = args[2];
+		String swipl = args[0];
+		String input_file = args[1];
+		String level_file = args[3];
+
+		// read level
+		BufferedReader is = null;
+		DataOutputStream os = null;
+		try {
+			is = new BufferedReader(new FileReader(level_file));
+			LevelMaker lm = new LevelMaker(is);
+			System.out.println(lm.level.toString());
+			// expecting .level file output .pl file
+			level_file = level_file.replace(".level", ".pl");
+			os = new DataOutputStream(new FileOutputStream(level_file));
+			os.writeBytes(":-include('"+input_file+"').");
+			os.writeByte('\n');
+			
+			lm.level.write(os);
+			
+			// append position caisse, position perso, predicat and co
+			os.writeByte('\n');
+			os.writeBytes("sokoban(ChSol):-sokoban(gd,["+lm.level.caisses+"],["+lm.level.cibles+"],[],"+NB_MAX_COUPS+",ChSol).");
+			os.writeByte('\n');
+			os.writeBytes(":- open('"+output_file+"',write,OS), sokoban(ChSol), write(OS,ChSol), halt.");
+			os.writeByte('\n');
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (is != null) {
+					is.close();
+				}
+				if (os != null) {
+					os.close();
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 
 		// progress bar thread
 		Thread progressBar = new Thread() {
@@ -37,13 +79,10 @@ public class Prolog {
 		};
 		progressBar.start();
 
-		// read level 
-		// TODO (je le ferais) call levelmaker and load .pl level file before sokoban.pl
-		
 		// prolog
 		try {
 			Process prologProcess = Runtime.getRuntime().exec(
-					SWIPL + " -f " + INPUT_FILE);
+					swipl + " -f " + level_file);
 			prologProcess.waitFor();
 			stopProgress = true;
 		} catch (Exception xx) {
@@ -51,18 +90,18 @@ public class Prolog {
 		}
 
 		// read solution
-		BufferedReader is = null;
+		BufferedReader is2 = null;
 		try {
-			is = new BufferedReader(new FileReader(OUTPUT_FILE));
-			System.out.println(is.readLine());
+			is2 = new BufferedReader(new FileReader(output_file));
+			System.out.println(is2.readLine());
 			// expecting .level file output .pl file
 
 		} catch (IOException e) {
 			e.printStackTrace();
 		} finally {
-			if (is != null)
+			if (is2 != null)
 				try {
-					is.close();
+					is2.close();
 				} catch (IOException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
